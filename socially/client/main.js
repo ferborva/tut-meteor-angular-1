fe2a1843efb99a7b82026d3c420d603d93aef692
ferborva <fbordallo2@gmail.com>
2016-10-22 16:49:49 +0200

    chore(): Step3. Include a dynamic template bound to a controller and a static model

--- socially/client/main.js
+++ socially/client/main.js
@@ -1,6 +1,23 @@
 import angular from 'angular';
 import angularMeteor from 'angular-meteor';
 
-angular.module('socially', [
+const app = angular.module('socially', [
   angularMeteor
 ]);
+
+app.controller('PartyListCtrl', function($scope){
+  'ngInject';
+
+  $scope.parties = [
+    {
+      'name': 'Dubstep-Free Zone',
+      'description': 'Can we please just for an evening not listen to dubstep.'
+    }, {
+      'name': 'All dubstep all the time',
+      'description': 'Get it on!'
+    }, {
+      'name': 'Savage lounging',
+      'description': 'Leisure suit required. And only fiercest manners.'
+    }
+  ];
+});
