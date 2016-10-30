362949144e29e3cdf7a4fb7ef7e7f4dffd9ba12d
ferborva <fbordallo2@gmail.com>
2016-10-30 17:35:00 +0100

    chore(): bootstrap app for phoneGap use

--- socially/client/main.js
+++ socially/client/main.js
@@ -1,2 +1,21 @@
-/* eslint-disable */
+import angular from 'angular';
+import { Meteor } from 'meteor/meteor';
+
 import { name as Socially } from '../imports/ui/components/socially/socially';
+
+function onReady(){
+	// eslint-disable-next-line
+	angular.bootstrap(document, [
+		Socially
+	], {
+		strictDi: true
+	});
+}
+
+if (Meteor.isCordova) {
+	// eslint-disable-next-line
+	angular.element(document).on('deviceReady', onReady);
+} else {
+	// eslint-disable-next-line
+	angular.element(document).ready(onReady);
+}
